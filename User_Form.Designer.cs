6506d1299146be1d461b86fa24b27c5bcc669bc7
khaba <khaba@DESKTOP-7KEI81B>
2025-06-10 09:48:45 +0500

    Add project files.

---
+++ User_Form.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WebAPI
+{
+    partial class User_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVUsers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVUsers
+            // 
+            this.DGVUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVUsers.Location = new System.Drawing.Point(25, 34);
+            this.DGVUsers.Margin = new System.Windows.Forms.Padding(2);
+            this.DGVUsers.Name = "DGVUsers";
+            this.DGVUsers.RowHeadersWidth = 51;
+            this.DGVUsers.RowTemplate.Height = 24;
+            this.DGVUsers.Size = new System.Drawing.Size(628, 275);
+            this.DGVUsers.TabIndex = 0;
+            // 
+            // User_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 366);
+            this.Controls.Add(this.DGVUsers);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "User_Form";
+            this.Text = "Users Form";
+            this.Load += new System.EventHandler(this.User_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVUsers;
+    }
+}
+
---
+++ User_Form.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace WebAPI
+{
+    public partial class User_Form : Form
+    {
+        public User_Form()
+        {
+            InitializeComponent();
+            Load += User_Form_Load;
+        }
+
+        private async void User_Form_Load(object sender, EventArgs e)
+        {
+            string url = "https://jsonplaceholder.typicode.com/users";
+
+            var client = new HttpClient();
+            string json = await client.GetStringAsync(url);
+
+            JArray array = JArray.Parse(json);
+
+            var userList = array.Select(user => new
+            {
+                Name = (string)user["name"],
+                Username = (string)user["username"],
+                Email = (string)user["email"]
+            }).ToList();
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Name");
+            table.Columns.Add("Username");
+            table.Columns.Add("Email");
+
+            foreach (var user in userList)
+            {
+                table.Rows.Add(user.Name,user.Username, user.Email);
+            }
+
+            DGVUsers.DataSource = table;
+        }
+    }
+
+}
